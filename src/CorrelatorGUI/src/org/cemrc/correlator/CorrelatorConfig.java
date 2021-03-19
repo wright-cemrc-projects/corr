84480fccad539a28d1b27ebb62062f07608c3246
Matt Larson <larsonmattr@gmail.com>
2021-03-19 12:41:53 -0500

    Fix for the updating of the window.

--- src/CorrelatorGUI/src/org/cemrc/correlator/CorrelatorConfig.java
+++ src/CorrelatorGUI/src/org/cemrc/correlator/CorrelatorConfig.java
@@ -9,7 +9,7 @@ import javafx.scene.image.Image;
  */
 public class CorrelatorConfig {
 	
-	public static String VersionName = "1.20.15";
+	public static String VersionName = "1.20.15.Development";
 	
 	public static String AppName = "CorRelator";
 	
--- src/CorrelatorGUI/src/org/cemrc/correlator/controllers/ImageRegistrationController.java
+++ src/CorrelatorGUI/src/org/cemrc/correlator/controllers/ImageRegistrationController.java
@@ -143,6 +143,7 @@ public class ImageRegistrationController {
 			
 			if (scale >= 0.0f && scale <= MAX_SCALE) {		
 				m_zoomPane.setScale(scale);
+				updateZoomCanvas();
 			}
 			
 		} catch (NumberFormatException ex) {
